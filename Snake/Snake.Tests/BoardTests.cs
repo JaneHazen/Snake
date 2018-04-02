9adefadf1a3997a0f17c7d892e6bff6c8f972fe8
v-janwil <iamjane@gmail.com>
2018-04-02 12:00:19 -0700

    Add test for Board

---
+++ Snake/Snake.Tests/BoardTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Snake.Tests
+{
+    [TestClass]
+    public class BoardTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // arrange
+            StringWriter str = new StringWriter();
+            Console.SetOut(str);
+            int expectedLength = Board.Boardheight * Board.Boardwidth;
+
+            // act
+           string actual = str.ToString();
+           int actualLength = actual.Length;
+
+
+            //assert
+            Assert.AreEqual(expectedLength, actualLength);
+        }
+    }
+}
